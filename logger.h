1c4488f56438b663a0356d6bd286c10761374306
iTitou <moiandme@gmail.com>
2014-10-05 14:14:27 +0200

    [add] LOGLVL_... constants

--- logger.h
+++ logger.h
@@ -1,11 +1,17 @@
 #ifndef DEFINE_LOGGER_HEADER
 #define DEFINE_LOGGER_HEADER
 
-#define LOG_LEVEL 3
+/* Default definition of the log level for whole program */
+#define LOG_LEVEL LOGLVL_INFO
 
-#define DEBUG(fmt, ...) if (LOG_LEVEL >= 4) printf("\033[1;34m (D) " fmt "\033[0m\n", ##__VA_ARGS__)
-#define INFO(fmt, ...) if (LOG_LEVEL >= 3) printf("\033[1;32m (I) " fmt "\033[0m\n", ##__VA_ARGS__)
-#define WARN(fmt, ...) if (LOG_LEVEL >= 2) printf("\033[1;33m (W) " fmt "\033[0m\n", ##__VA_ARGS__)
-#define ERROR(fmt, ...) if (LOG_LEVEL >= 1) printf("\033[1;31m (E) " fmt "\033[0m\n", ##__VA_ARGS__)
+#define LOGLVL_ERROR 1
+#define LOGLVL_WARN  2
+#define LOGLVL_INFO  3
+#define LOGLVL_DEBUG 4
+
+#define DEBUG(fmt, ...) if (LOG_LEVEL >= LOGLVL_DEBUG) printf("\033[1;34m (D) " fmt "\033[0m\n", ##__VA_ARGS__)
+#define INFO(fmt, ...) if (LOG_LEVEL >= LOGLVL_INFO) printf("\033[1;32m (I) " fmt "\033[0m\n", ##__VA_ARGS__)
+#define WARN(fmt, ...) if (LOG_LEVEL >= LOGLVL_WARN) printf("\033[1;33m (W) " fmt "\033[0m\n", ##__VA_ARGS__)
+#define ERROR(fmt, ...) if (LOG_LEVEL >= LOGLVL_ERROR) printf("\033[1;31m (E) " fmt "\033[0m\n", ##__VA_ARGS__)
 
 #endif
