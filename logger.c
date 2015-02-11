68c43effeb09bfaca008d35a316eceae5b34a18d
iTitou <moiandme@gmail.com>
2015-02-11 21:24:12 +0100

    [enh] Add time to log messages

--- logger.c
+++ logger.c
@@ -1,6 +1,7 @@
 #include "logger.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <time.h>
 
 int current_log_level = LOG_LEVEL;
 
@@ -12,7 +13,14 @@ void print_log(int lvl, const char *fmt, ... )
     if (LOGLVL_ERROR <= lvl && lvl <= current_log_level){
         va_list args;
         va_start(args, fmt);
-        printf("\033[1;3%dm(%c)\033[0m ", level_color[lvl], level_prefix[lvl]);
+
+        time_t now_seconds = time(NULL);
+        struct tm *now = localtime(&now_seconds);
+
+        printf("\033[1;3%dm(%c) %02d-%02d-%02d %02d:%02d:%02d\033[0m ", 
+               level_color[lvl], level_prefix[lvl],
+               now->tm_year + 1900, now->tm_mon + 1, now->tm_mday,
+               now->tm_hour, now->tm_min, now->tm_sec);
         vprintf(fmt, args);
         printf("\n");
     }
